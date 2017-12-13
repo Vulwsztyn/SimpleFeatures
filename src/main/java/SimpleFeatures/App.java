5eea91a4a105b1927442b084e2d564b1945e9276
Jędrzej Potoniec <Jedrzej.Potoniec@cs.put.poznan.pl>
2017-12-13 10:17:24 +0100

    SPARQLquery interfejsem, JenaSPARQLquery jego implementacja

--- src/main/java/SimpleFeatures/App.java
+++ src/main/java/SimpleFeatures/App.java
@@ -8,7 +8,7 @@ public class App
     public static void main(String[] args) {
         String inputHTTP="http://dbpedia.org/ontology/waterwayThroughTunnel";
         String inputQuery = "select ?s ?o where {?s <"+inputHTTP+"> ?o} order by ?o ?s";
-        SPARQLquery stringRows = new SPARQLquery(inputQuery);
+        SPARQLquery stringRows = new JenaSPARQLquery(inputQuery);
         TableOfPairs sortedByO = new TableOfPairs(stringRows);
         TableOfPairs sortedByS = new TableOfPairs(sortedByO);
         sortedByS.sortByS();
--- src/main/java/SimpleFeatures/SPARQLquery.java
+++ src/main/java/SimpleFeatures/SPARQLquery.java
@@ -1,53 +1,11 @@
 package SimpleFeatures;
-import org.apache.jena.query.* ;
-import org.apache.jena.rdf.model.RDFNode;
-import org.apache.jena.rdf.model.Resource;
-import org.apache.jena.sparql.engine.http.QueryEngineHTTP ;
-import java.util.*;
 
-public class SPARQLquery
-{
-    private  ArrayList<SOrow> rows=new ArrayList<>();
+import java.util.List;
 
-    public SPARQLquery(String SPARQLendpoint)
-    {
-        Query query = QueryFactory.create(SPARQLendpoint);
+public interface SPARQLquery {
 
+    List<SOrow> getRows();
 
-        // Remote execution.
-        try ( QueryExecution qexec = QueryExecutionFactory.sparqlService("http://dbpedia.org/sparql", query) ) {
-
-
-            // Set the DBpedia specific timeout.
-            ((QueryEngineHTTP)qexec).addParam("timeout", "10000") ;
-
-
-            // Execute.
-            ResultSet rs = qexec.execSelect();
-            List<String> vars = rs.getResultVars();
-            assert vars.size() == 2;
-            while(rs.hasNext()) {
-                QuerySolution solution=rs.nextSolution();
-                Resource s = solution.getResource(vars.get(0));
-                RDFNode o = solution.get(vars.get(1));
-                rows.add(new SOrow(s, o));
-            }
-
-
-
-        }
-
-        catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public List<SOrow> getRows() {
-        return rows;
-    }
-
-    public int getNumberOfRows(){
-        return rows.size();
-    }
+    int getNumberOfRows();
 
 }
